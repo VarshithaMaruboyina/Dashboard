fc0a14d51a1ad6d2719d0e30c0fb06ef82d3f50a
varshitha <varshithamaruboyina@gmail.com>
2021-05-26 16:20:09 +0530

    change6

--- internship/urls.py
+++ internship/urls.py
@@ -18,7 +18,6 @@ from django.urls import path,include
 from django.shortcuts import render,redirect
 from . import views
 from django.conf import settings
-from django.conf.urls.static import static
 from django.views.static import serve
 from django.conf.urls import url
 
@@ -27,6 +26,4 @@ urlpatterns = [
     path("",include("dashboard.urls")),
     path("",include("notes.urls")),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
